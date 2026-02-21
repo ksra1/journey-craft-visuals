13ce95477fb1b54e966b186b7f0c82f202d808fb
Sravan <sravan.kollapudi@gmail.com>
2026-02-21 18:34:10 -0500

    Make pages work

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/journey-craft-visuals/">
+      <BrowserRouter basename="/journey-craft-visuals">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
